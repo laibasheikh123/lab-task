86e225aa65f09e1aa78c079ee3c9c7629b970398
laibasheikh123 <150238601+laibasheikh123@users.noreply.github.com>
2023-11-08 14:41:50 +0500

    Add files via upload

---
+++ apk.js
@@ -0,0 +1,20 @@
+
+// let numstd=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//     for (let i = 0; i < numstd.length; i++) {
+//         if(numstd[i] % 2 == 0  )
+//     console.log(numstd[i])
+//     }
+
+// let numstd=[2, 5, 6, 8, 2]
+// let sum= 0
+
+//     for (let i = 0; i < numstd.length; i++) 
+
+//         if(numstd[i] % 2 == 0  )
+//         {
+
+//  sum=sum+numstd[i]
+
+//     }
+//     console.log(sum)
